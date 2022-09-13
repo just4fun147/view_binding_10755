4d2f4677fb5d452b3601667eaa2238bd1a489efe
just4fun147 <pauluswindito1@gmail.com>
2022-09-13 14:25:31 +0700

    Initial commit

---
+++ app/src/main/java/com/example/view_binding_10755/TaskList.kt
@@ -0,0 +1,12 @@
+package com.example.view_binding_10755
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("Chapter 1", "Bangun Tidur",
+            "Kebiasaan sehari-hari untuk membuat hari makin Indah"),
+        Task("Chapter 2", "Nguli Genshin Impact","Mencari Primo buat Gacha "),
+        Task("Chapter 3", "Ngopi","Habis capek"),
+        Task("Chapter 4", "Test","Lanjut"),
+        Task("Chapter 5", "Upload","Akhir")
+    )
+}
